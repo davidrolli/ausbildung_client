f21aaef6efbba1445bbf592cd99fda78f414e26f
David Rolli <david.rolli@hotmail.com>
2014-02-15 16:08:49 +0100

    Sync vom 15.2.2014

--- app/router.js
+++ app/router.js
@@ -1,13 +1,12 @@
 App.Router.map(function() {
 
-/*
   this.resource("dokumente", function() {
     this.resource("dokument", { path: "/id/:dokument_id" });
   });
-*/
 
+/*
   this.resource("dokumente");
-
+*/
   this.resource("werke");
 
   this.resource("autoren");
